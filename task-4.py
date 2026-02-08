28a8d0e313144f8f87a45acd0aa2a2d1da64a1cf
Ruslan <info@mirasov.dev>
2026-02-08 01:22:12 +0100

    create get_upcoming_birthdays function

---
+++ task-4.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+
+def get_upcoming_birthdays(users):
+   today = datetime.today().date()
+   upcoming_birthdays = []
+
+   for user in users:
+      birthday_date = datetime.strptime(user["birthday"], "%Y.%m.%d").date()
+      birthday_this_year = birthday_date.replace(year=today.year)
+
+      if birthday_this_year < today:
+         birthday_this_year = birthday_this_year.replace(year=today.year + 1)
+
+      days_until_birthday = (birthday_this_year - today).days
+
+      if 0 <= days_until_birthday <= 7:
+         birthday_week_day = birthday_this_year.weekday()
+         congratulation_date = birthday_this_year
+
+         if birthday_week_day == 5:
+            congratulation_date += timedelta(days=2)
+         elif birthday_week_day == 6:
+            congratulation_date += timedelta(days=1)
+
+         upcoming_birthdays.append({
+            "name" : user["name"], 
+            "congratulation_date": congratulation_date.strftime("%Y.%m.%d")
+         })
+
+   return sorted(upcoming_birthdays, key=lambda item: item["congratulation_date"])  
+
+#test
+users = [
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.02.12"},
+    {"name": "Ben Gus", "birthday": "1982.02.08"},
+]
+
+upcoming_birthdays = get_upcoming_birthdays(users)
+print("Список привітань на цьому тижні:", upcoming_birthdays)
